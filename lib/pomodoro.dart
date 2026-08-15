1e155b7bed5b93d9adcb20a4e6c7e2e94c627984
Tasdiqq <tasdiqulislam2017@icloud.com>
2026-08-15 22:11:51 +0600

    Initial Color Palette Selection
    for pomodoro timer page

--- lib/pomodoro.dart
+++ lib/pomodoro.dart
@@ -1,1 +1,15 @@
-//Here the pomodoro Timer will be implemented
+import 'package:flutter/material.dart';
+
+class PomodoroColors {
+  static const Color base = Color(0xFF645887);
+  static const Color base_white = Color(0xFFFDF7FF);
+  static const Color base_container = Color(0xFFD3C4FA);
+  static const Color base_container2 = Color(0xFF483D69);
+  static const Color blue = Color(0xFF5743DF);
+  static const Color white = Color(0xFFFEF7FF);
+  static const Color light_white1 = Color(0xFFF4EAFF);
+  static const Color light_white2 = Color(0xFFEADDFF);
+  static const Color dark_purple = Color(0xFF362E4B);
+  static const Color light_purple = Color(0xFF645A7A);
+  static const Color border = Color(0xFF807597);
+}
